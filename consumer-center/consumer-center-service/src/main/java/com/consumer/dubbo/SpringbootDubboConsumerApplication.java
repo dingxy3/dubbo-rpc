0ecce44afb85de8c2eb1cc74a138fddffad9b411
dingxy <xiaoyu.ding@ymm56.com>
2018-09-21 18:28:34 +0800

    web

--- consumer-center/consumer-center-service/src/main/java/com/consumer/dubbo/SpringbootDubboConsumerApplication.java
+++ consumer-center/consumer-center-service/src/main/java/com/consumer/dubbo/SpringbootDubboConsumerApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -16,11 +18,19 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource("dubbo-consumer.xml")
 @ComponentScan(basePackages = {"com.dubbo","com.consumer.dubbo"})
-public class SpringbootDubboConsumerApplication {
+public class SpringbootDubboConsumerApplication extends SpringBootServletInitializer {
     private static Logger logger = LoggerFactory.getLogger(SpringbootDubboConsumerApplication.class);
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return configureApplication(builder);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDubboConsumerApplication.class,args);
         logger.error("恭喜你启动成功了！--SpringbootDubboConsumerApplication 启动成功...");
+    }
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootDubboConsumerApplication.class);
     }
 }
